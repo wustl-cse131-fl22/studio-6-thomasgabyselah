75eb20cb04777e20441181e63439b1eda3822f09
gabye <gabye@DESKTOP-D58TJKL.dhcp.wustl.edu>
2022-10-27 14:19:55 -0500

    Unfinished studio 6

--- src/studio6/RecursiveMethods.java
+++ src/studio6/RecursiveMethods.java
@@ -14,7 +14,18 @@ public class RecursiveMethods {
 	public static double geometricSum(int n) {
 		
 			// FIXME compute the geometric sum for the first n terms recursively
+		if (n == 0) {
 			return 0;
+		}
+		else {
+			return geometricSum(n-1) + Math.pow(0.5, n);
+		}
+//		double sum = 0.0;
+//		for (int power = 1; power <= n; ++power) {
+//			sum = sum + Math.pow(0.5, power);
+//		}
+//		return sum;
+//			return 0;
 		
 	}
 
@@ -29,8 +40,20 @@ public class RecursiveMethods {
 	public static int gcd(int p, int q) {
 		
 			// FIXME compute the gcd of p and q using recursion
-			return 0;
-		
+		if (q == 0) {
+			return p;
+		}
+		else 
+			return gcd(q, p%q);
+			
+			
+//			while (q != 0) {
+//				int temp = q;
+//				q = p % q;
+//				p = temp;
+//			}
+//			return p;
+//		
 	}
 
 	
@@ -41,10 +64,42 @@ public class RecursiveMethods {
 	 * @param array the array to create a reverse of, not to be mutated
 	 * @return an array with the same data as the input but it reverse order
 	 */
+	public static int[] helperMethod(int[] array, int index) {
+		if (index <= array.length /2) {
+			return array;
+		}
+		else {
+			int temp = array[index];
+			array[index] = array[(array.length - 1) - index];
+			array[(array.length - 1) - index] = array[index];
+			return helperMethod(array, index);
+		}
+	}
+	
 	public static int[] toReversed(int[] array) {
 		
 			// FIXME create a helper method that can recursively reverse the given array
+		
+		if (array.length == 0) {
 			return new int[0];
+		}
+		else {
+			return toReversed(array);
+		}
+			
+//			int[] reversed = new int[array.length];
+//			if (array.length > 0) {
+//				int lastIndex = array.length - 1;
+//				for (int index = 0; index <= array.length / 2; ++index) {
+//					int mirrorIndex = lastIndex - index;
+//					// note:
+//					// since we read from array and write to reversed
+//					// we do not need to use a temp variable
+//					reversed[index] = array[mirrorIndex];
+//					reversed[mirrorIndex] = array[index];
+//				}
+//			}
+//			return reversed;
 		
 	}
 
